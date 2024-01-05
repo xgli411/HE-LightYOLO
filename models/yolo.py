985ccba5a07af8e87a3026be6e67857815080200
xgli411 <148942023+xgli411@users.noreply.github.com>
2024-01-05 13:48:16 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -204,7 +204,7 @@ class DetectionModel(BaseModel):
             s = 256  # 2x min stride
             m.inplace = self.inplace
             forward = lambda x: self.forward(x)[0] if isinstance(m, Segment) else self.forward(x)
-            m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(2, ch, s, s))])  # forward  改成2，原为1
+            m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(2, ch, s, s))])
             check_anchor_order(m)
             m.anchors /= m.stride.view(-1, 1, 1)
             self.stride = m.stride
